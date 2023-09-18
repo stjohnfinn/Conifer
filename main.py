40578dc8c9b191f26627b8101aaa7925c3f211bb
Finn St John <stjohn.finn@gmail.com>
2023-09-17 21:36:59 -0700

    add first few files

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import random
+
+#
+# Generate Extremely Basic Data   
+#
+
+DATA_dir = 'data'
+DATA_size = 10_000
+
+# returns an entire row as a string
+def create_data_point():
+  x = random.randint(0, 10_000)
+  y = 2 * x + 1
+  return f"{x}, {y}"
+
+with open(f'{DATA_dir}/basic_data.csv', 'w+') as data_file:
+  # header
+  data_file.write('x,y\n')
+
+  # generate data according to create_data_point
+  for i in range(DATA_size):
+    line = create_data_point() + '\n'
+    data_file.write(line)
+
+print('Generated data!')
