5ba82375ba7db2661527ef60216a91f53503f115
Finn St John <stjohn.finn@gmail.com>
2023-09-24 00:18:40 -0700

    start work on loading, convert to jupyter notebook

--- n_dimensional.py
+++ n_dimensional.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 import numpy as np
-from perceptron import PerceptronNd
+import pandas as pd
 
 #
 # LOAD DATA
 #
 
 DATA_DIR = 'data'
+DATA_FILE = 'neo_v2.csv'
+DATA_FILEPATH = f'{DATA_DIR}/{DATA_FILE}'
 
+data = pd.read_csv(DATA_FILEPATH, header=0)
